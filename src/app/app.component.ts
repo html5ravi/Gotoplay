dd15c094534c47f5d0183f334921922dd1b862f6
Ravi G <ravi.g@spurtreetech.com>
2018-06-01 19:33:01 +0530

    Working on the court sharing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -75,6 +75,22 @@ export class MyApp {
 
       // stop disconnect watch
       disconnectSubscription.unsubscribe();
+      // watch network for a connection
+let connectSubscription = this.network.onConnect().subscribe(() => {
+  alert('network connected!');
+  // We just got a connection but we need to wait briefly
+   // before we determine the connection type. Might need to wait.
+  // prior to doing any api requests as well.
+  setTimeout(() => {
+    if (this.network.type === 'wifi') {
+      alert('we got a wifi connection, woohoo!');
+    }
+  }, 3000);
+});
+
+// stop connect watch
+connectSubscription.unsubscribe();
+
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
--- src/pages/court-sharing/court-sharing.ts
+++ src/pages/court-sharing/court-sharing.ts
@@ -14,7 +14,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'court-sharing.html',
 })
 export class CourtSharingPage {
-    
+    contributors:any=[
+        {
+            name:"Ravi G",
+            id:1,
+            toPayAmount:1050,
+            img:"assets/img/profile/200x200kobe.png"
+        },{
+            name:"Noble A",
+            id:2,
+            toPayAmount:450,
+            img:"assets/img/profile/200x200kobe.png"
+        },{
+            name:"Guru",
+            id:3,
+            toPayAmount:1200,
+            img:"assets/img/profile/200x200kobe.png"
+        },{
+            name:"Raghavkumar",
+            id:4,
+            toPayAmount:2500,
+            img:"assets/img/profile/200x200kobe.png"
+        },
+    ];
+    currentDates:any=[
+        {
+            date:"12/05/2018",
+            playedAmount:450,
+            contributors:[1,2,3,4],
+            shuttleBy:"Ravi"
+        },
+        {
+            date:"11/05/2018",
+            playedAmount:300,
+            contributors:[1,2,3,4],
+            shuttleBy:"Noble"
+        },
+        {
+            date:"10/05/2018",
+            playedAmount:450,
+            contributors:[1,2,3,4],
+            shuttleBy:"Arun"
+        },
+        {
+            date:"09/05/2018",
+            playedAmount:280,
+            contributors:[1,2,3,4],
+            shuttleBy:"Guru"
+        }
+    ];
+  
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     
   }
@@ -22,5 +72,11 @@ export class CourtSharingPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad CourtSharingPage');
   }
+
+
+  toggleSection(i) {
+    this.currentDates[i].open = !this.currentDates[i].open;
+  } 
+  
 
 }
